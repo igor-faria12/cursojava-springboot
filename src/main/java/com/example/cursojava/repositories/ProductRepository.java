fbb4132723f8dec66ba466d15c48de08517549ce
Igor Faria <igorfaria@discente.ufg.br>
2021-05-20 11:23:05 -0300

    user insert

--- src/main/java/com/example/cursojava/repositories/ProductRepository.java
+++ src/main/java/com/example/cursojava/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.cursojava.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.cursojava.entities.Category;
 import com.example.cursojava.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
